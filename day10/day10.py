6eea3bc1d96a3ce29f9642d381e5a238d4ea2665
Odarchuk Artem <a.m.odarchuk@gmail.com>
2024-11-26 14:13:47 +0200

    day10

---
+++ day10/day10.py
@@ -0,0 +1,187 @@
+test_input = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+# lines = test_input.splitlines()
+lines = open("input.txt", "r").read().splitlines()
+
+cycle = 1
+x = 1
+goal_cycles = [20, 60, 100, 140, 180, 220]
+goal_signals = []
+
+screen = [["."] * 40 for _ in range(6)]
+
+for line in lines:
+    # print(cycle, x)
+    xs = (cycle - 1) % 40
+    ys = (cycle - 1) // 40
+    if abs(xs - x) <= 1:
+        # print(xs, ys)
+        screen[ys][xs] = "#"
+    cycle += 1
+    if cycle in goal_cycles:
+        goal_signals.append(x * cycle)
+    if line == "noop":
+        pass
+    else:
+        # print(cycle, x)
+        xs = (cycle - 1) % 40
+        ys = (cycle - 1) // 40
+        if abs(xs - x) <= 1:
+            screen[ys][xs] = "#"
+        _, n = line.split()
+        cycle += 1
+        x += int(n)
+        if cycle in goal_cycles:
+            goal_signals.append(x * cycle)
+
+res1 = sum(goal_signals)
+
+
+print(res1)
+print("\n".join("".join(line) for line in screen))
+
